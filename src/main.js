42bb20ec42deeb03e2e6cc3bd6696214a22671bf
Yaypixel <mryaypixel@gmail.com>
2025-09-26 00:00:49 -0400

    alot of cool stuff

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Stats from 'three/addons/libs/stats.module.js'
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
 import RAPIER from '@dimforge/rapier3d-compat'
 import { FirstPersonControls } from 'three/examples/jsm/Addons.js'
+import { degToRad, radToDeg } from 'three/src/math/MathUtils.js'
 
 await RAPIER.init()
 const gravity = new RAPIER.Vector3(0.0, -20, 0.0)
@@ -19,7 +20,6 @@ const settings = {
   shadowMapSize: 2056,
   shadowRadius: 5
 }
-
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000)
 camera.position.set(0, 2, 5)
@@ -55,9 +55,6 @@ directionalLight.shadow.camera.top = d;
 directionalLight.shadow.camera.bottom = - d;
 scene.add(directionalLight);
 
-const controls = new FirstPersonControls(camera, renderer.domElement)
-controls.lookSpeed = 0.02
-controls.movementSpeed = 5
 
 const cubeMesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 0x00ff00}))
 cubeMesh.castShadow = true
@@ -67,11 +64,11 @@ const cubeShape = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5).setMass(2).setRestit
 world.createCollider(cubeShape, cubeBody)
 dynamicBodies.push([cubeMesh, cubeBody])
 
-const ballMesh = new THREE.Mesh(new THREE.SphereGeometry(0.5), new THREE.MeshNormalMaterial)
+const ballMesh = new THREE.Mesh(new THREE.SphereGeometry(0.2), new THREE.MeshNormalMaterial)
 ballMesh.castShadow = true
 scene.add(ballMesh)
 const ballBody = world.createRigidBody(RAPIER.RigidBodyDesc.dynamic().setTranslation(-2, 5, 0).setCanSleep(false).setLinearDamping(0.5).setAngularDamping(1))
-const ballShape = RAPIER.ColliderDesc.ball(0.5).setMass(1).setMass(0.5).setRestitution(1.1)
+const ballShape = RAPIER.ColliderDesc.ball(0.2).setMass(1).setMass(0.5).setRestitution(0.5)
 world.createCollider(ballShape, ballBody)
 dynamicBodies.push([ballMesh, ballBody])
 
@@ -82,6 +79,9 @@ scene.add(targetMesh)
 const targetBody = world.createRigidBody(RAPIER.RigidBodyDesc.fixed().setTranslation(-2, 3, -30))
 const targetShape = RAPIER.ColliderDesc.cuboid(2, 5, 0.5)
 world.createCollider(targetShape, targetBody)
+
+camera.position.set(-2, 2, -30)
+camera.rotation.y = degToRad(180)
 
 const floorMesh = new THREE.Mesh(new THREE.BoxGeometry(100, 1, 300), new THREE.MeshPhongMaterial())
 floorMesh.receiveShadow = true
@@ -96,15 +96,13 @@ const mouse = new THREE.Vector2()
 
 const exitVelo = {
   x: 0,
-  y: 0,
-  z: 40
+  y: 25,
+  z: 50
 }
 
 document.addEventListener('keydown', function(e) {
   if (e.key === 'e') {
-    ballBody.setLinvel(new RAPIER.Vector3(0, 0, 0), true)
-    ballBody.setTranslation(new RAPIER.Vector3(-2, 5, 50), true)
-    ballBody.applyImpulse(new RAPIER.Vector3(0, 0, -80), true)
+    
   }
 
   if (e.key === 'r') {
@@ -149,26 +147,7 @@ ballFolder.add(exitVelo, 'z', 0, 100, 1)
 const clock = new THREE.Clock()
 let delta
 
-function animate() {
-  requestAnimationFrame(animate)
 
-  delta = clock.getDelta()
-  world.timestep = Math.min(delta, 0.1)
-  world.step()
-
-  for (let i = 0, n = dynamicBodies.length; i < n; i++) {
-    dynamicBodies[i][0].position.copy(dynamicBodies[i][1].translation())
-    dynamicBodies[i][0].quaternion.copy(dynamicBodies[i][1].rotation())
-  }
-
-  controls.update(0.1)
-
-  renderer.render(scene, camera)
-
-  stats.update()
-}
-
-animate()
 
 const shadowsFolder = gui.addFolder('Shadows')
 shadowsFolder.add( settings, 'shadowMapType', ["basic", "pcf","pcfsoft", "vsm"])
@@ -206,3 +185,122 @@ shadowsFolder.add( settings, 'shadowRadius', 0, 100, 1 )
     directionalLight.shadow.radius = value;
     directionalLight.shadow.updateMatrices(directionalLight);
 })
+
+let pci = document.getElementById("pci")
+let ball = document.getElementById("ball")
+let ballShadow = document.getElementById("ballShadow")
+
+
+let moving = false
+let image = null
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+let time1
+let time2
+let strike = false
+let handness = 1  // 1 = righty, -1 = lefty
+
+pci.addEventListener("mousedown", function () {
+  let pciBox = pci.getBoundingClientRect()
+  let ballBox = ball.getBoundingClientRect()
+    if (!(pciBox.right < ballBox.left || pciBox.left > ballBox.right || pciBox.bottom < ballBox.top || pciBox.top > ballBox.bottom)) {
+      time2 = performance.now() % 1000
+      
+      if ((time2 - time1) < 0) {
+        if (time1 >= 800) {
+          time1 = 100 - (time1 - 1000)
+        }
+      }
+
+      let xdir = handness * (75 - Math.abs(time2 - time1)) / 2
+      console.log(time2, time1)
+      console.log(xdir)
+      console.log("hit in " + Math.abs(time2 - time1) % 1000)
+      ballBody.setLinvel(new RAPIER.Vector3(0, 0, 0), true)
+      ballBody.setTranslation(new RAPIER.Vector3(-2, 2, -29), true)
+      ballBody.applyImpulse(new RAPIER.Vector3(xdir, exitVelo.y, exitVelo.z))
+    }
+    else {
+      console.log("miss")
+      
+    }
+}) 
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.addEventListener("keydown", async function (e) {
+    if (e.code === "Space") {
+        ballBody.setLinvel(new RAPIER.Vector3(0, 0, 0), true)
+        ballBody.setTranslation(new RAPIER.Vector3(-2, 5, 50), true)
+        ballBody.applyImpulse(new RAPIER.Vector3(0, 0, -80), true)
+        let top = 50 //getRandomInt(20, 80)
+        let left = 50 //getRandomInt(20, 80)
+        ballShadow.style.display = 'inline'
+        ball.style.display = 'none'
+        ballShadow.style.top = `${top}%`
+        ballShadow.style.left = `${left}%`
+        await sleep(450)
+        ball.style.display = 'inline'
+        ballShadow.style.display = 'none'
+        ball.style.top = `${top}%`
+        ball.style.left = `${left}%`
+        time1 = performance.now() % 1000
+        await sleep(150)
+        ball.style.display = 'none'
+
+        if (top <= 70 && top >= 30 && left <= 70 && left >= 30) {
+            strike = true
+        }
+        else {
+            strike = false
+        }
+    }
+
+})
+
+function move(e) {
+  document.body.style.cursor = "none";
+
+  let newX = e.clientX - 10;
+  let newY = e.clientY - 10;
+
+  image.style.left = newX + "px";
+  image.style.top = newY + "px";
+}
+
+function initialClick(e) {
+
+  moving = true;
+  image = this;
+
+  document.addEventListener("mousemove", move, false);
+}
+
+pci.addEventListener("mousedown", initialClick, false);
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  delta = clock.getDelta()
+  world.timestep = Math.min(delta, 0.1)
+  world.step()
+
+  for (let i = 0, n = dynamicBodies.length; i < n; i++) {
+    dynamicBodies[i][0].position.copy(dynamicBodies[i][1].translation())
+    dynamicBodies[i][0].quaternion.copy(dynamicBodies[i][1].rotation())
+  }
+
+
+  renderer.render(scene, camera)
+
+  stats.update()
+}
+
+animate()
--- src/pci/pci.js
+++ src/pci/pci.js
@@ -1,6 +1,86 @@
-$(document).ready(function() {
-    $(document).mousemove(function(e) {
-        $('img').css('left',e.pageX+"px")
-        $('img').css('top',e.pageY+"px")
-    })
+let pci = document.getElementById("pci")
+let ball = document.getElementById("ball")
+let ballShadow = document.getElementById("ballShadow")
+
+
+let moving = false
+let image = null
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+let time1
+let time2
+let strike = false
+
+pci.addEventListener("mousedown", function () {
+  let pciBox = pci.getBoundingClientRect()
+  let ballBox = ball.getBoundingClientRect()
+    if (!(pciBox.right < ballBox.left || pciBox.left > ballBox.right || pciBox.bottom < ballBox.top || pciBox.top > ballBox.bottom)) {
+      time2 = performance.now() % 1000
+      
+      if ((time2 - time1) < 0) {
+        time1 = Math.abs(time1 - 1000)
+      }
+      console.log(time1, time2)
+      console.log("hit in " + (time2 - time1) % 1000)
+    }
+    else {
+      console.log("miss")
+      
+    }
+}) 
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.addEventListener("keydown", async function (e) {
+    if (e.code === "Space") {
+        let top = getRandomInt(20, 80)
+        let left = getRandomInt(20, 80)
+        ballShadow.style.display = 'inline'
+        ball.style.display = 'none'
+        ballShadow.style.top = `${top}%`
+        ballShadow.style.left = `${left}%`
+        await sleep(1000)
+        ball.style.display = 'inline'
+        ballShadow.style.display = 'none'
+        ball.style.top = `${top}%`
+        ball.style.left = `${left}%`
+        time1 = performance.now() % 1000
+        await sleep(200)
+        ball.style.display = 'none'
+
+        if (top <= 70 && top >= 30 && left <= 70 && left >= 30) {
+            strike = true
+        }
+        else {
+            strike = false
+        }
+    }
+
 })
+
+function move(e) {
+  document.body.style.cursor = "none";
+
+  let newX = e.clientX - 10;
+  let newY = e.clientY - 10;
+
+  image.style.left = newX + "px";
+  image.style.top = newY + "px";
+}
+
+function initialClick(e) {
+
+  moving = true;
+  image = this;
+
+  document.addEventListener("mousemove", move, false);
+}
+
+pci.addEventListener("mousedown", initialClick, false);
